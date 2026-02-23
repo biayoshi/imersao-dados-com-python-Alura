d0de837a80cdf4eaf50b7f71efb03e6d89f0b8cc
Beatriz Yoshikawa <beatrizpitangacosta@gmail.com>
2026-02-23 18:22:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,7 @@ df_filtrado = df[
 # Conteúdo Principal
 st.title("🎲 Dashboard - Analisando Salários na Área de Dados")
 st.markdown("Explore os dados salariais na área de dados nos últimos anos! Utilize os filtros à esquerda para refinar a análise!")
+st.markdown("Importante: Lembre-se de deixar ao menos um filtro marcado por categoria!")
 
 # Métricas Principais (KPIs)
 st.subheader("Métricas gerais/KPIs (Salário anual em USD)")
@@ -233,3 +234,4 @@ with col_graf4:
 st.subheader("Dados Detalhados")
 
 st.dataframe(df_filtrado)
+
